0257571b4a44ba1714071af40e9114497610270a
kkumara3 <kkumara3@cisco.com>
2016-07-28 21:31:23 +0000

    added comments to grpc client

--- client/cisco_grpc_client.py
+++ client/cisco_grpc_client.py
@@ -4,6 +4,22 @@ import ems_grpc_pb2
 
 class CiscoGRPCClient(object):
     def __init__(self, host, port, timeout, user, password, creds=None, options=None):
+        """This class creates grpc calls using python.
+            :param username: Username for device login
+            :param password: Password for device login
+            :param host: The ip address for the device
+            :param port: The port for the device
+            :param timeout: how long before the rpc call timesout
+            :param creds: Input of the pem file
+            :param options: TLS server name
+            :type password: str
+            :type username: str
+            :type server: str
+            :type port: int
+            :type timeout:int
+            :type creds: str
+            :type options: str
+        """
         if creds != None:
             self._target = '%s:%d' % (host, port)
             self._creds = implementations.ssl_channel_credentials(creds)
@@ -19,7 +35,23 @@ class CiscoGRPCClient(object):
         self._timeout = int(timeout)
         self._metadata = [('username', user), ('password', password)]
 
+    def __repr__(self):
+        return '%s(Host = %s, Port = %s, User = %s, Password = %s, Timeout = %s)' % (
+            self.__class__.__name__,
+            self._host,
+            self._port,
+            self._metadata[0][1],
+            self._metadata[1][1],
+            self._timeout
+        )
+
     def getconfig(self, path):
+        """Get grpc call
+            :param data: JSON
+            :type data: str
+            :return: Return the response object
+            :rtype: Response stream object
+        """
         message = ems_grpc_pb2.ConfigGetArgs(yangpathjson=path)
         responses = self._stub.GetConfig(message,self._timeout, metadata = self._metadata)
         objects = ''
@@ -28,6 +60,12 @@ class CiscoGRPCClient(object):
         return objects
 
     def get_subscription(self, subid):
+        """Telemetry subscription function
+            :param data: Name of subscription
+            :type: string
+            :return: Returns telemetry stream
+            :rtype: Response object
+        """
         sub_args = ems_grpc_pb2.CreateSubsArgs(ReqId=1, encode=3, subidstr=subid)
         stream = self._stub.CreateSubs(sub_args, timeout=self._timeout, metadata=self._metadata)
         self._channel.subscribe(self.connectivity_handler, True)
@@ -38,16 +76,34 @@ class CiscoGRPCClient(object):
         print result
 
     def mergeconfig (self, yangjson):
+        """Merge grpc call equivalent  of PATCH RESTconf call
+            :param data: JSON
+            :type data: str
+            :return: Return the response object
+            :rtype: Response object
+        """
         message = ems_grpc_pb2.ConfigArgs(yangjson= yangjson)
         response = self._stub.MergeConfig(message, self._timeout, metadata = self._metadata)
         return response
 
     def deleteconfig (self, yangjson):
+        """delete grpc call
+            :param data: JSON
+            :type data: str
+            :return: Return the response object
+            :rtype: Response object
+        """
         message = ems_grpc_pb2.ConfigArgs(yangjson= yangjson)
         response = self._stub.DeleteConfig(message, self._timeout, metadata = self._metadata)
         return response
 
     def replaceconfig (self, yangjson):
+        """Replace grpc call equivalent of PUT in restconf
+            :param data: JSON
+            :type data: str
+            :return: Return the response object
+            :rtype: Response object
+        """
         message = ems_grpc_pb2.ConfigArgs(yangjson= yangjson)
         response= self._stub.ReplaceConfig(message, self._timeout, metadata = self._metadata)
         return response

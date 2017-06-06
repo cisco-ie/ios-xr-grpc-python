58de28428474d18946aeae82584b3b75f5e532ba
kkumara3 <kkumara3@cisco.com>
2017-06-06 09:18:22 -0700

    Initial commit for packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='iosxr_grpc',
+      version='1.0',
+      description='gRPC library for IOS-XR > 6.1.1',
+      url='https://github.com/cisco-grpc-connection-libs/ios-xr-grpc-python',
+      author='Karthik Kumaravel',
+      authoer_email='srirudrankumaravel@gmail.com',
+      licencse='Apache 2.0',
+      packages=['iosxr_grpc'],
+      zip_safe=False)

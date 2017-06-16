569b76c060765c4a849d80f35217785c57af8872
Karthik Kumaravel <kkumara3@cisco.com>
2017-06-16 11:59:41 -0700

    Update setup.py (#13)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='iosxr_grpc',
-      version='1.1',
+      version='1.2',
       description='gRPC library for IOS-XR > 6.1.1',
       url='https://github.com/cisco-grpc-connection-libs/ios-xr-grpc-python',
       author='Karthik Kumaravel',
@@ -10,6 +10,6 @@ setup(name='iosxr_grpc',
       licencse='Apache 2.0',
       packages=['iosxr_grpc'],
       install_requires=[
-          'grpcio==1.1.0',
+          'grpcio==1.0.0',
       ],
       zip_safe=False)
